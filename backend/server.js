a4bf2419b0f24d9128f82d9998e45cd120ada8e6
Mohammadreza Tahzbibi <m.r.tahzibi@gmail.com>
2022-09-09 18:55:36 +0430

    Minor update

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV === 'production') {
  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
  // FIX: below code fixes app crashing on refresh in deployment
- app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+ app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
  });
 } else {
  app.get('/', (req, res) => {
